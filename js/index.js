d9b67ab154f186f6ea8c0e0057d618ee2a116508
mrcinzza <mrcinzza@gmail.com>
2021-12-07 18:52:43 +0100

    small adjust

--- js/index.js
+++ js/index.js
@@ -7,17 +7,17 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         console.log('Its a Tie');
     } else if (playerSelection === opts[0] && computerSelection === opts[2]) {
-        console.log('You win!');
+        console.log('You win! Rock beats scissors');
     } else if (playerSelection === opts[0] && computerSelection === opts[1]) {
-        console.log('You Loose!')
+        console.log('You Loose! Paper beats Rock')
     } else if (playerSelection === opts[1] && computerSelection === opts[2]) {
-        console.log('You Loose!')
+        console.log('You Loose! Scissors beat paper')
     } else if (playerSelection === opts[1] && computerSelection === opts[0]) {
-        console.log('You win!');
+        console.log('You win! Paper beats Rock');
     } else if (playerSelection === opts[2] && computerSelection === opts[0]) {
-        console.log('You Loose!')
+        console.log('You Loose! Rock beats scissors')
     } else if (playerSelection === opts[2] && computerSelection === opts[1]) {
-        console.log('You win!')
+        console.log('You win! Scissors beats Paper')
     }
 };
 
