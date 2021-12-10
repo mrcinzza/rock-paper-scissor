dbcd4f594fdb8d0f9ad2135ed0971ee4a7c7a39d
mrcinzza <mrcinzza@gmail.com>
2021-12-10 10:24:28 +0100

    Big Changes to the game

--- js/index.js
+++ js/index.js
@@ -1,23 +1,39 @@
 var opts = ['rock', 'paper', 'scissors'];
-
-
+let whoWins = Boolean;
+let computerScore = 0;
+let playerScore = 0;
 
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        console.log('Its a Tie');
-    } else if (playerSelection === opts[0] && computerSelection === opts[2]) {
-        console.log('You win! Rock beats scissors');
-    } else if (playerSelection === opts[0] && computerSelection === opts[1]) {
-        console.log('You Loose! Paper beats Rock')
-    } else if (playerSelection === opts[1] && computerSelection === opts[2]) {
-        console.log('You Loose! Scissors beat paper')
-    } else if (playerSelection === opts[1] && computerSelection === opts[0]) {
-        console.log('You win! Paper beats Rock');
-    } else if (playerSelection === opts[2] && computerSelection === opts[0]) {
-        console.log('You Loose! Rock beats scissors')
-    } else if (playerSelection === opts[2] && computerSelection === opts[1]) {
-        console.log('You win! Scissors beats Paper')
+    if (playerSelection.toLowerCase() === computerSelection) {
+        let str = 'Its a Tie';
+        computerScore++;
+        playerScore++;
+        output(str, playerScore);
+    } else if (playerSelection.toLowerCase() === opts[0] && computerSelection === opts[2]) {
+        let str = 'You win! Rock beats scissors';
+        playerScore++;
+        output(str, playerScore);
+    } else if (playerSelection.toLowerCase() === opts[0] && computerSelection === opts[1]) {
+        let str = 'You Loose! Paper beats Rock';
+        computerScore++;
+        output(str, playerScore);
+    } else if (playerSelection.toLowerCase() === opts[1] && computerSelection === opts[2]) {
+        let str = 'You Loose! Scissors beat paper';
+        computerScore++;
+        output(str, playerScore);
+    } else if (playerSelection.toLowerCase() === opts[1] && computerSelection === opts[0]) {
+        let str = 'You win! Paper beats Rock';
+        playerScore++;
+        output(str, playerScore);
+    } else if (playerSelection.toLowerCase() === opts[2] && computerSelection === opts[0]) {
+        let str = 'You Loose! Rock beats scissors';
+        computerScore++;
+        output(str, playerScore);
+    } else if (playerSelection.toLowerCase() === opts[2] && computerSelection === opts[1]) {
+        let str = 'You win! Scissors beats Paper';
+        playerScore++;
+        output(str, playerScore);
     }
 };
 
@@ -25,15 +41,33 @@ function playRound(playerSelection, computerSelection) {
 let computerPlay = () => opts[Math.floor(Math.random() * opts.length)];
 
 const computerSelection = computerPlay();
-const playerSelection = "";
+let playerSelection = "";
 
-var round = 1;
+var round = 0;
 
 let game = () => {
+
     for (; round < 5; round++) {
+        playerSelection = prompt('Rock, Paper or Scissor?');
+        playRound(playerSelection, computerSelection);
+    }
 
-
+    if (playerScore === computerScore){
+        let str = 'THE GAME ENDS WITH A TIE!';
+        output(str, playerScore);
+    } else if ( playerScore > computerScore) {
+        let str = '**** YOU WIN THE GAME! ****';
+        output(str, playerScore);
+    } else {
+        let str = 'YOU LOOSE THE GAME!';
+        output(str, playerScore);
     }
 }
 
-console.log(playRound(playerSelection, computerSelection));
+let output = ( str, scre ) => {
+    const para = document.createElement('p');
+    document.body.appendChild(para);
+    para.textContent = str+ ', Score: ' +scre;
+}
+
+game();
